5da612073a25f4914e9087843d7189288003a5b5
kappaMtech02001 <me23mtech02001@iith.ac.in>
2024-04-20 21:05:19 +0530

    runs multiple files

--- run_abaqus.cmd
+++ run_abaqus.cmd
@@ -1,4 +1,6 @@
 @echo off
+
+echo  ----------  ---------- Starting Script ----------  ---------- 
 setlocal enabledelayedexpansion
 
 @REM Define path to abaqus executable.
@@ -6,7 +8,6 @@ set abaqus_path="abaqus.bat"
 set numCPUs=2
 set numGPUs=0
 set interactiveFlag=true
-set job_name=test
 set userSubroutine=
 set delete_files=true
 
@@ -57,36 +58,54 @@ for /L %%i in (1,1,%argCount%) do (
     )
     
 )
+set "tab=    "
+echo The following input files will be processed:
+for %%f in (*.inp) do (
+    if "%%~xf"==".inp" (
+    echo  !tab! %%~nf
+    )
+    )
+set /p "choice=Would you like to continue? (Y/N): "
+if /i "%choice%"=="N" (  goto :exit_script )
+if /i not "%choice%"=="Y" ( goto :exit_script)
+for %%f in (*.inp) do (
+    if "%%~xf"==".inp" (
 
+        set job_name=%%~nf
+        echo ---------- starting job  !job_name! ----------
+        @REM Basic abaqus command 
+        set abaqus_cmd=!abaqus_path! job=!job_name! cpus=!numCPUs!
 
+        @REM Check if GPUs are to be included
+        if !numGPUs! gtr 0 (
+            set abaqus_cmd=!abaqus_cmd! gpus=!numGPUs!
+        )
 
-@REM Basic abaqus command 
-set abaqus_cmd=%abaqus_path% job=%job_name% cpus=%numCPUs%
+        @REM Check if interactive mode is enabled
+        if /I "!interactiveFlag!"=="true" (
+            set abaqus_cmd=!abaqus_cmd! int
+        )
 
-@REM Check if GPUs are to be included
-if %numGPUs% gtr 0 (
-    set abaqus_cmd=%abaqus_cmd% gpus=%numGPUs%
+        @REM Check if ask_delete is OFF mode is enabled
+        if /I "!delete_files!"=="true" (
+            set abaqus_cmd=!abaqus_cmd! ask_delete=OFF
+        )
+
+        @REM Check if user subroutine is provided
+        if not "!userSubroutine!"=="" (
+            set abaqus_cmd=!abaqus_cmd! user="!userSubroutine!"
+        )
+        echo Executing the following commad
+        echo !abaqus_cmd!
+
+        @REM Runs the prepared abaqus commad
+        call  !abaqus_cmd!
+
+	)
 )
 
-@REM Check if interactive mode is enabled
-if /I "%interactiveFlag%"=="true" (
-    set abaqus_cmd=%abaqus_cmd% int
-)
+:exit_script
 
-@REM Check if ask_delete is OFF mode is enabled
-if /I "%delete_files%"=="true" (
-    set abaqus_cmd=%abaqus_cmd% ask_delete=OFF
-)
+echo ----------  ---------- Done Executing Script ----------  ----------
 
-@REM Check if user subroutine is provided
-if not "%userSubroutine%"=="" (
-    set abaqus_cmd=%abaqus_cmd% user="%userSubroutine%"
-)
-echo Executing the following commad
-echo %abaqus_cmd%
-
-@REM Runs the prepared abaqus commad
-call  %abaqus_cmd%
-
-echo Done Executing Script
-@REM exit 0
+exit 0
